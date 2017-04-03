4d2e2e3d0bd772fb5dd18c715d1476fda2224559
Rory Claasen <rory@claasen.biz>
2017-04-03 22:08:34 +0100

    adding travis support

---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o errexit
+
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+cp README.md build/web
+cp LICENSE build/web/license.txt
+
+cd build/web
+git init
+git add --a
+git commit -m "Deploy to Github Pages"
+git push --force --quiet "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages
