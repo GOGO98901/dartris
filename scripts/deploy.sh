5b5a9e8aed66e03b417ac0294bde061e19ff274e
Rory Claasen <rory@claasen.biz>
2017-04-03 22:19:28 +0100

    move the addition of teh deploy key

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,11 +14,6 @@ git config --global user.name "Travis CI"
 cp README.md build/web
 cp LICENSE build/web/license.txt
 
-cd build/web
-git init
-git add --a
-git commit -m "Deploy to Github Pages: ${SHA}"
-
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
@@ -27,5 +22,10 @@ openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out 
 chmod 600 deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
+
+cd build/web
+git init
+git add --a
+git commit -m "Deploy to Github Pages: ${SHA}"
 
 git push --force --quiet $SSH_REPO $TARGET_BRANCH
